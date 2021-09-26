027a2a63a8e1b80d1621b43a20cdb50de63f62e5
HudsonOnHere <wright.richard94@icloud.com>
2021-09-26 17:40:47 -0400

    exception handler working

--- weatherbot.py
+++ weatherbot.py
@@ -2,21 +2,12 @@
 
 from email import message
 import os
-from unicodedata import name
 from dotenv import load_dotenv
 import requests as r
 import datetime
 from telebot import TeleBot
 
 load_dotenv()
-
-class ConnectionError(Exception):
-    def __init__(self, error, message="Connection interrupted, reconnecting..."):
-        self.error = error
-        self.message = message
-        super().__init__(self.message)
-        
-
 
 # YOU WILL NEED YOUR OWN API KEYS TO GET THIS WORKING
 API_KEY = os.getenv('API_KEY')
@@ -32,7 +23,6 @@ data = r.get(url).json()
 # gets us the date & time
 today = datetime.datetime.now().strftime('%A, %B %dth, %Y')
 time = datetime.datetime.now().strftime('%H:%M') # will be utilized in next version
-
 
 
 # gets the specific data the bot will send as a string
@@ -73,12 +63,12 @@ def repeat(message):
     msg = "I only know /weather and /help. And yes, those commands are case-sensitive." #.format(user_msg) # this will include their message in the bot's reply
     bot.send_message(chat_dest, msg)
 
+# keeps bot running even if connection is interrupted
 if __name__ == '__main__':
     bot.config['api_key'] = BOT_KEY
     try:
         bot.poll(debug=True)
-    except ValueError as e:
-        raise ConnectionError(message)
+    except Exception as e:
+        pass
     finally:
         bot.poll(debug=True)
-
